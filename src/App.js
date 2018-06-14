43eab6dd4aced781ea3baaddc2732be0c3bb3a22
Evgeny <gazdovsky@gmail.com>
2018-06-15 00:14:40 +0300

    address input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import React, { Component } from 'react';
+import { TextField } from 'rmwc/TextField';
+import 'material-components-web/dist/material-components-web.min.css';
 
 export default class extends Component {
+  state = {};
+
+  go = (event) => {
+    this.setState({ src: event.target.value });
+  }
+
   render() {
+    const { src } = this.state;
     return (
       <div className="container">
-        <webview id="foo" src="https://www.github.com/" style={{ width: '100%', height: '100%' }} />
+        <div className="toolbar">
+          <TextField value={src} onChange={this.go} fullwidth />
+        </div>
+        <webview src={src} style={{ width: '100%', height: '100%' }} plugins />
       </div>
     );
   }
