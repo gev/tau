c4e05d0d8d6a1396d0049d12a539a3b6822ace5a
Evgeny <gazdovsky@gmail.com>
2018-06-27 04:09:34 +0300

    refactor history

--- src/actions/history.js
+++ src/actions/history.js
@@ -4,24 +4,29 @@ import { set } from './create';
 import u from './util';
 
 export const go = (url) => (dispatch) => {
-  dispatch(set(HISTORY, { url }));
+  dispatch(set(HISTORY, { url, shift: 0 }));
 };
 
 export const push = (url) => (dispatch, getState) => {
   const history = getState()[POOL][HISTORY];
   if (history) {
-    const { index = -1, stack = [] } = history;
-    if (stack[index] === url) return;
-    dispatch(u.set(HISTORY, {
-      url,
-      index: index + 1,
-      stack: [...stack.slice(0, index + 1), url]
-    }));
+    const { index = 0, stack = [], shift = 1 } = history;
+    if (stack[index] === url) {
+      dispatch(u.set(HISTORY, { url, shift: 1 }));
+    } else {
+      dispatch(u.set(HISTORY, {
+        url,
+        index: index + shift,
+        stack: [...stack.slice(0, index + shift), url],
+        shift: 1
+      }));
+    }
   } else {
     dispatch(u.set(HISTORY, {
       url,
       index: 0,
-      stack: [url]
+      stack: [url],
+      shift: 1
     }));
   }
 };
@@ -31,7 +36,8 @@ export const goBack = () => (dispatch, getState) => {
   if (history && history.index > 0) {
     dispatch(u.set(HISTORY, {
       url: history.stack[history.index - 1],
-      index: history.index - 1
+      index: history.index - 1,
+      shift: 0
     }));
   }
 };
@@ -41,7 +47,8 @@ export const goForward = () => (dispatch, getState) => {
   if (history && history.stack && history.index < history.stack.length - 1) {
     dispatch(u.set(HISTORY, {
       url: history.stack[history.index + 1],
-      index: history.index + 1
+      index: history.index + 1,
+      shift: 0
     }));
   }
 };
